adc15f238ff6eec37ab73422442b007c5d36bd58
Marcin Starmach <maricha177@gazeta.pl>
2017-09-13 12:25:24 +0200

    pwa

--- run.sh
+++ run.sh
@@ -9,6 +9,11 @@ cp node_modules/@angular/service-worker/bundles/worker-basic.min.js dist/
 
 ./node_modules/.bin/ngu-sw-manifest --module src/app/app.module.ts --out dist/ngsw-manifest.json
 
+./node_modules/.bin/ngu-app-shell --module src/app/app.module.ts \
+              --url /loading \
+              --insert-module src/app/loading.module.ts \
+              --out dist/index.html
+
 #serve
 cd dist
 http-server
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
+import { NgServiceWorker, ServiceWorkerModule } from '@angular/service-worker'
+
 import { AppComponent } from './app.component';
 import { appRouter } from './app.router';
 
@@ -13,11 +15,18 @@ import { appRouter } from './app.router';
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({appId: 'ng-store'}),
     NoopAnimationsModule,
     HttpClientModule,
+    ServiceWorkerModule,
     appRouter
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(sw: NgServiceWorker) {
+    sw.push.subscribe(() => {
+      console.log("notyfikacja")
+    })
+  }
+}
